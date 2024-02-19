2a5a2fc10f26182f553a10946a8be3dd9c923f16
Nasser Ben <nasserbenyed@gmail.com>
2024-02-19 00:55:43 -0800

    add create workout template with edit/delete from async storage

--- frontend/components/createWorkout.js
+++ frontend/components/createWorkout.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { View, Text, Button, TextInput, ScrollView } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const CreateWorkout = () => {
+const CreateWorkout = ({ templateUpdated, setTemplateUpdated }) => {
   const [templateName, setTemplateName] = useState("");
-  const [exercises, setExercises] = useState([{ name: "" }]);
+  const [exercises, setExercises] = useState([]);
 
   const handleExerciseChange = (index, text) => {
     const newExercises = [...exercises];
-    newExercises[index].name = text;
+    newExercises[index] = text;
     setExercises(newExercises);
   };
 
   const addExerciseField = () => {
-    setExercises([...exercises, { name: "" }]);
+    setExercises([...exercises, ""]);
   };
 
   const removeExerciseField = (index) => {
@@ -21,11 +22,36 @@ const CreateWorkout = () => {
     setExercises(newExercises);
   };
 
-  const saveTemplate = () => {
-    console.log("Template Name:", templateName);
-    console.log("Exercises:", exercises);
-    // Implement your logic to save the template to a database or local storage
+  const saveTemplate = async () => {
+    if (templateName && exercises.every((exercise) => exercise)) {
+      const newTemplate = {
+        templateName: templateName,
+        exercises: exercises.filter((exercise) => exercise.trim() !== ""),
+      };
+      try {
+        // await AsyncStorage.clear();
+        let updatedTemplates = [];
+        const storedTemplates = await AsyncStorage.getItem("workoutTemplates");
+        if (storedTemplates !== null) {
+          updatedTemplates = JSON.parse(storedTemplates);
+        }
+        updatedTemplates.push(newTemplate);
+        await AsyncStorage.setItem(
+          "workoutTemplates",
+          JSON.stringify(updatedTemplates)
+        );
+        setTemplateUpdated(!templateUpdated);
+        setTemplateName("");
+        setExercises([]);
+        console.log("Template saved successfully:", newTemplate);
+      } catch (error) {
+        console.error("Error saving template:", error);
+      }
+    } else {
+      console.log("Template name or exercises are empty");
+    }
   };
+
   return (
     <ScrollView>
       <Text className="text-black font-bold text-2xl m-4">Create Workout</Text>
@@ -59,7 +85,11 @@ const CreateWorkout = () => {
         className="bg-blue-500 text-white p-2 rounded m-4"
         title="Save Template"
         onPress={saveTemplate}
-        disabled={!templateName || exercises.some((exercise) => !exercise.name)}
+        disabled={
+          !templateName ||
+          exercises.some((exercise) => !exercise) ||
+          exercises.length < 1
+        }
       />
     </ScrollView>
   );
--- frontend/screens/createTab.js
+++ frontend/screens/createTab.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView } from "react-native";
 import CreateWorkout from "../components/createWorkout";
 import MyWorkouts from "../components/myWorkouts";
 
 export default function CreateTab() {
+  const [templateUpdated, setTemplateUpdated] = useState(false);
   return (
     <ScrollView className="flex-1 mt-14">
-      <CreateWorkout />
-      <MyWorkouts />
+      <CreateWorkout
+        templateUpdated={templateUpdated}
+        setTemplateUpdated={setTemplateUpdated}
+      />
+      <MyWorkouts templateUpdated={templateUpdated} />
     </ScrollView>
   );
 }
