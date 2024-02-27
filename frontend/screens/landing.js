50ccd587cf065d5c89477a11915149aa93260bc9
Maliha Fatima <malihameer@hotmail.com>
2024-02-27 15:37:00 -0800

    updated bg

--- frontend/screens/landing.js
+++ frontend/screens/landing.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Landing = ({ navigation }) => {
   const handleOnPress = () => {
     navigation.navigate("SignUp");
   };
   return (
-    <View className="flex-1 justify-center items-center bg-white">
+   
+    <View className="flex-1">
+       <LinearGradient
+    colors={["rgba(56, 163, 165, 0.25)", "rgba(128, 237, 153, 0.75)"]}
+    style={{ flex: 1 , justifyContent: "center", alignItems: "center" }}  
+    >
       <Text className="text-2xl mb-8 font-bold">Fitness Tracker</Text>
       <TouchableOpacity
         onPress={handleOnPress}
@@ -14,7 +20,9 @@ const Landing = ({ navigation }) => {
       >
         <Text className="text-white text-lg">Get Started</Text>
       </TouchableOpacity>
-    </View>
+      </LinearGradient>
+         </View>
+      
   );
 };
 
