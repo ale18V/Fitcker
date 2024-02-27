d845f8437c983da07484b78054f23b08e1d2ef32
Maliha Fatima <malihameer@hotmail.com>
2024-02-27 15:55:52 -0800

    minor changes

--- frontend/screens/landing.js
+++ frontend/screens/landing.js
@@ -13,10 +13,10 @@ const Landing = ({ navigation }) => {
     colors={["rgba(56, 163, 165, 0.25)", "rgba(128, 237, 153, 0.75)"]}
     style={{ flex: 1 , justifyContent: "center", alignItems: "center" }}  
     >
-      <Text className="text-2xl mb-8 font-bold">Fitness Tracker</Text>
+      <Text className="text-2xl mb-8 text-teal-700 font-bold mt-60">Fitness Tracker</Text>
       <TouchableOpacity
         onPress={handleOnPress}
-        className="bg-custom-teal px-6 py-2 rounded-lg"
+        className="bg-teal-600 px-6 py-2 rounded-lg mt-40"
       >
         <Text className="text-white text-lg">Get Started</Text>
       </TouchableOpacity>
