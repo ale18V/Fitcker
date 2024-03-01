74f632c2cff4641cfe6c79d4bbf4ce9c03798fc9
ale18V <alebombarda@icloud.com>
2024-02-29 22:13:22 -0800

    Restructure code

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,31 +1,59 @@
+from datetime import timedelta
 from typing import Annotated
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from model import User
-from sqlmodel import Session, select, insert
+from sqlmodel import Session, select
 import db
-
-app = FastAPI()
-
-
-@app.post("/api/v1/user/register")
-async def register(user: User):
-    con = db.connect()
-    res = con.execute(select(User).where(User.email == user.email)).one_or_none()
-    if res:
-        return {"status": "error", "msg": "Email already used"}
-
-    con.execute(
-        insert(User).values(name=user.name, email=user.email, password=user.password)
-    )
-    con.commit()
-    con.close()
-    return {"status": "success", "msg": "User registered successfully"}
+from security import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    Token,
+    authenticate_user,
+    create_access_token,
+)
 
 
-@app.post("/api/v1/user/login")
-async def login(user: User):
-    with Session(db.engine) as session:
-        if session.exec(select(User).where(User.name == user.name)).one_or_none():
+def create_app(db_url=None):
+    app = FastAPI()
+
+    @app.on_event("startup")
+    async def setup_db():
+        db.create_tables()
+
+    @app.post("/api/v1/user", status_code=201)
+    async def register(user: User, con: Annotated[Session, Depends(db.get_session)]):
+
+        res = con.exec(select(User).where(User.email == user.email)).one_or_none()
+        if res:
+            raise HTTPException(status_code=409, detail="User already registered")
+
+        con.add(user)
+        con.commit()
+        con.refresh(user)
+        return user
+
+    @app.post("/api/v1/user/login")
+    async def login(user: User, con: Annotated[Session, Depends(db.get_session)]):
+        if con.exec(select(User).where(User.name == user.name)).one_or_none():
             return {"status": "success", "msg": "User logged in"}
         else:
             return {"status": "error", "msg": "Wrong credentials"}
+
+    @app.post("/token")
+    async def login_for_access_token(
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+    ) -> Token:
+        user = authenticate_user(form_data.username, form_data.password)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        access_token = create_access_token(
+            data={"sub": user.username}, expires_delta=access_token_expires
+        )
+        return Token(access_token=access_token, token_type="bearer")
+
+    return app
--- backend/src/db.py
+++ backend/src/db.py
@@ -1,26 +1,23 @@
 from os import getenv
 from sqlmodel import create_engine, SQLModel, Session
 
+
 conf = {
     "user": getenv("MYSQL_USER"),
     "password": getenv("MYSQL_PASSWORD"),
     "host": getenv("MYSQL_HOST"),
     "database": getenv("MYSQL_DATABASE"),
-    "debug": getenv("DEBUG") is not None,
+    "debug": getenv("DEBUG") or True,
 }
-
 
 db_url = f"mysql+mysqlconnector://{conf['user']}:{conf['password']}@{conf['host']}/{conf['database']}"
 engine = create_engine(db_url, echo=conf["debug"])
 
 
-def connect():
-    return engine.connect()
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
-def create_db_and_tables():  # Want to run it only once
-    # Move to init.py
+def create_tables():  # Want to run it only once
     SQLModel.metadata.create_all(engine)
-
-
-create_db_and_tables()
