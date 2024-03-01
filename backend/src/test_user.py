95624103bfd3b418fc27c843b20365765a011f8b
ale18V <alebombarda@icloud.com>
2024-02-29 22:14:03 -0800

    Add test for user registration

---
+++ backend/src/test_user.py
@@ -0,0 +1,49 @@
+from fastapi.testclient import TestClient
+from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel.pool import StaticPool
+from app import create_app
+import pytest
+from db import get_session
+
+app = create_app()
+client = TestClient(app)
+
+
+@pytest.fixture(name="session")
+def session_fixture():
+    engine = create_engine(
+        "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
+    )
+    SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        yield session
+
+
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+def register(user, passw, email):
+    resp = client.post(
+        "/api/v1/user", json={"name": user, "email": email, "password": passw}
+    )
+    return resp
+
+
+def test_register(client: TestClient):
+    user, passw, email = "foo", "bar", "foo@bar.com"
+    resp = register(user, passw, email)
+    data = resp.json()
+    assert resp.status_code == 201
+    assert data["id"] is not None
+    assert data["name"] == user
+    assert data["password"] == passw
+    assert data["email"] == email
