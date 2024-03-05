a6bcf04ea365130c69c81a49189cfef550080239
Maliha Fatima <malihameer@hotmail.com>
2024-03-04 18:01:13 -0800

    test landing page

--- frontend/screens/test.js
+++ frontend/screens/test.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent} from '@testing-library/react-native';
 import Landing from './landing';
+
 
 describe('Landing Page', () => {
   it('renders correctly', () => {
     const { toJSON } = render(<Landing />);
     expect(toJSON()).toMatchSnapshot();
   });
+
+  it('renders "Get Started" button and handles press event', () => {
+    const navigation = { navigate: jest.fn() };
+    const { getByText } = render(<Landing navigation={navigation} />);
+    const getStartedButton = getByText('Get Started');
+    fireEvent.press(getStartedButton);
+    expect(navigation.navigate).toHaveBeenCalledWith('SignUp');
+  });
+
 });
+
