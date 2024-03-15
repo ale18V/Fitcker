445b64bdc6174f1fe4b239c45e7612576edad916
Nasser Ben <nasserbenyed@gmail.com>
2024-03-15 00:38:45 -0700

    main-> nasserben/dev

--- frontend/screens/statsTab.jsx
+++ frontend/screens/statsTab.jsx
@@ -1,36 +1,32 @@
 import * as React from "react";
-<<<<<<< HEAD
 import StatsCalendar from "../components/statsCalendar";
 import StatsGraphs from "../components/statsGraphs";
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 const Tab = createMaterialTopTabNavigator();
 
 export default function StatsTab() {
   return (
-      <Tab.Navigator className = "mt-9" screenOptions={{
-        tabBarActiveTintColor: '#38A3A5',
+    <Tab.Navigator
+      className="mt-9"
+      screenOptions={{
+        tabBarActiveTintColor: "#38A3A5",
         tabBarInactiveTintColor: "#A3A3A3",
-        tabBarIndicatorStyle: { backgroundColor: '#38A3A5'},
-    
-      }}>
-        <Tab.Screen name="Graphs" component={(props) => (
-          <StatsGraphs 
+        tabBarIndicatorStyle: { backgroundColor: "#38A3A5" },
+      }}
+    >
+      <Tab.Screen
+        name="Graphs"
+        component={(props) => (
+          <StatsGraphs
             {...props}
             statsGraph={{
-              exercise: "Bench Press", 
+              exercise: "Bench Press",
             }}
           />
-        )} />
-        <Tab.Screen name="Calendar" component={StatsCalendar} />
-      </Tab.Navigator>
-=======
-import { Text, View } from "react-native";
-import Stats from "../components/stats";
-
-export default function StatsTab() {
-  return (
-    <Stats></Stats>
->>>>>>> 7f417859d388d9c2c0b5fc62f0e088f25e5e931c
+        )}
+      />
+      <Tab.Screen name="Calendar" component={StatsCalendar} />
+    </Tab.Navigator>
   );
 }
