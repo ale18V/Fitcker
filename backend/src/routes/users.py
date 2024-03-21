956f502ff084ec460e82585e6e1d1b471caf0202
ale18V <alebombarda@icloud.com>
2024-03-21 10:09:10 -0700

    Add additional responses to openapi docs

--- backend/src/routes/users.py
+++ backend/src/routes/users.py
@@ -1,7 +1,9 @@
 from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException, status
 from typing import Annotated
+from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import BaseModel
 from sqlmodel import Session, select
 from sqlalchemy.exc import IntegrityError
 import db
@@ -12,8 +14,13 @@ import security
 router = APIRouter(prefix="/users")
 
 
-@router.post("/", status_code=201, response_model=UserRead)
-async def register(user: UserCreate, con: Annotated[Session, Depends(db.get_session)]) -> User:
+class Message(BaseModel):
+    msg: str
+
+
+@router.post("/", status_code=201, response_model=UserRead,
+             responses={409: {"model": Message}})
+async def register(user: UserCreate, con: Annotated[Session, Depends(db.get_session)]):
     db_user: User = User.model_validate(user)
     db_user.password = security.get_password_hash(user.password)
     con.add(db_user)
@@ -21,24 +28,25 @@ async def register(user: UserCreate, con: Annotated[Session, Depends(db.get_sess
         con.commit()
     except IntegrityError:
         con.rollback()
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
-                            detail="Username or mail already taken")
+        return JSONResponse(status_code=status.HTTP_409_CONFLICT,
+                            content=Message(msg="Username or mail already taken").model_dump())
     con.refresh(db_user)
     return db_user
 
 
-@router.post("/login", response_model=security.Token)
+@router.post("/login", response_model=security.Token,
+             responses={400: {"model": Message}})
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-                con: Annotated[Session, Depends(db.get_session)]) -> security.Token:
+                con: Annotated[Session, Depends(db.get_session)]):
 
     query = select(User) \
         .where(User.username == form_data.username)
     user = con.exec(query).one_or_none()
 
     if not user or not security.verify_password(form_data.password, user.password):
-        raise HTTPException(
+        return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Incorrect username or password",
+            content=Message(msg="Incorrect username or password").model_dump(),
             headers={"WWW-Authenticate": "Bearer"},
         )
 
@@ -47,11 +55,12 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     return security.Token(access_token=access_token, token_type="bearer")
 
 
-@router.get("/me", response_model=UserRead)
+@router.get("/me", response_model=UserRead,
+            responses={404: {"model": Message}})
 async def current_user(user_id: Annotated[int, Depends(security.get_current_user_id)],
                        con: Annotated[Session, Depends(db.get_session)]):
     user = con.get(User, user_id)
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                            content=Message(msg="User not found").model_dump())
     return user
