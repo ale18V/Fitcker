20f0c55fcf7e53a4dcc8cadb46d30dfe23871f76
ale18V <alebombarda@icloud.com>
2025-02-02 16:44:15 +0100

    Fix nativewind

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,9 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-	    "babel-preset-expo",
-	    "@babel/preset-typescript"
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+      "@babel/preset-typescript"
     ],
-    plugins: ["nativewind/babel"],
   };
 };
