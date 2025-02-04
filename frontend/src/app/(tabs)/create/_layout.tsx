bd19711d6ba22460572f1084dc10a94cba88a563
ale18V <alebombarda@icloud.com>
2025-02-04 22:39:08 +0100

    Fix insets

--- frontend/src/app/(tabs)/create/_layout.tsx
+++ frontend/src/app/(tabs)/create/_layout.tsx
@@ -1,18 +1,21 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { withLayoutContext } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const { Navigator } = createMaterialTopTabNavigator()
 const Tab = withLayoutContext(Navigator);
 
 export default function CreateTab() {
+  const insets = useSafeAreaInsets()
   return (
     <Tab
-      className="mt-9"
       screenOptions={{
         tabBarActiveTintColor: "#38A3A5",
         tabBarInactiveTintColor: "#A3A3A3",
         tabBarIndicatorStyle: { backgroundColor: '#38A3A5'},
-    
+        tabBarStyle: {
+            paddingTop: insets.top
+        }
       }}/>
   );
 }
--- frontend/src/app/(tabs)/stats/_layout.tsx
+++ frontend/src/app/(tabs)/stats/_layout.tsx
@@ -1,10 +1,12 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { withLayoutContext } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const { Navigator } = createMaterialTopTabNavigator()
 const Tab = withLayoutContext(Navigator);
 
 export default function StatsTab() {
+  const insets = useSafeAreaInsets()
   return (
     <Tab
       className="mt-9"
@@ -12,7 +14,9 @@ export default function StatsTab() {
         tabBarActiveTintColor: "#38A3A5",
         tabBarInactiveTintColor: "#A3A3A3",
         tabBarIndicatorStyle: { backgroundColor: '#38A3A5'},
-    
+        tabBarStyle: {
+            paddingTop: insets.top
+        }
       }}/>
   );
 }
